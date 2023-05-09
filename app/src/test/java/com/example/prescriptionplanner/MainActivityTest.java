c77a30d4555c539d69f7c1004469a6cfe6f18bcc
rinawata <42815681+rinawata@users.noreply.github.com>
2023-05-09 09:33:53 -0700

    1.4 added test cases to MainActivity.java, DatabaseHandler.java, and SplashActivity.java to ensure functions are working properly

---
+++ app/src/test/java/com/example/prescriptionplanner/MainActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.prescriptionplanner;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+
+    public void testHandleDialogClose() {
+    }
+}
---
+++ app/src/test/java/com/example/prescriptionplanner/SplashActivityTest.java
@@ -0,0 +1,9 @@
+package com.example.prescriptionplanner;
+
+import junit.framework.TestCase;
+
+public class SplashActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+}
